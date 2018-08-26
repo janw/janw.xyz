7e17675602a5e42a596362eeeb3e8ba74cccfc10
Jan Willhaus <mail@janwillhaus.de>
2018-08-26 10:38:17 +0200

    Make GitHub API token unbound, to include in .zshrc[.local]

--- fa_icons_download.sh
+++ fa_icons_download.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-set -e
+set -eu
 
 icons="light/user-astronaut light/address-card light/rss light/pen-fancy brands/mastodon brands/github brands/linkedin brands/instagram brands/flickr brands/keybase solid/envelope brands/telegram brands/apple light/heart light/copyright solid/comment-alt-dots solid/key light/comments"
 
 dest="themes/janwxyz/static/icons"
 
-GITHUB_API_TOKEN="###SECRET###"
 GITHUB_OWNER="FortAwesome"
 GITHUB_REPO="Font-Awesome-Pro"
 repo_subdir="https://raw.githubusercontent.com/$GITHUB_OWNER/$GITHUB_REPO/master/advanced-options/raw-svg"
