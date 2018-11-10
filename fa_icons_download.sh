e068d997d86e41bb0f1ef568ab4fa3897f305369
Jan Willhaus <mail@janwillhaus.de>
2018-11-10 13:47:58 +0100

    Extended theme, added minipage berlin apartment hunting

--- fa_icons_download.sh
+++ fa_icons_download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-icons="light/user-astronaut light/address-card light/rss light/pen-fancy brands/mastodon brands/github brands/linkedin brands/instagram brands/flickr brands/keybase solid/envelope brands/telegram brands/apple light/heart light/copyright solid/comment-alt-dots solid/key light/comments"
+icons="light/user-astronaut light/address-card light/rss light/pen-fancy brands/mastodon brands/github brands/linkedin brands/instagram brands/flickr brands/keybase solid/envelope brands/telegram brands/apple light/heart light/copyright solid/comment-alt-dots solid/key light/comments light/ruler-combined light/door-open light/map-marker-smile light/shower light/moon-stars light/usd-circle light/flame light/people-carry light/sun light/utensils light/phone light/envelope-open-text light/bullhorn brands/whatsapp light/hand-holding-heart"
 
 dest="themes/janwxyz/static/icons"
 
@@ -17,6 +17,6 @@ for icon in $icons; do
 
   curl --header "Authorization: token $GITHUB_API_TOKEN" \
        --header "Accept: application/vnd.github.v3.raw" \
-       --remote-name \
+       --remote-name -J \
        --location "${repo_subdir}/${icon}"
 done
