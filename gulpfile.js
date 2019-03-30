cac10cf5d250fd9471db83869167616dc7fa5554
Jan Willhaus <mail@janwillhaus.de>
2019-03-30 19:53:11 +0100

    Config cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,35 @@ var gulp         = require("gulp"),
 var srcp = "themes/janwxyz/src",
     dest = "themes/janwxyz/static";
 
-gulp.task("scss", function () {
+
+var compileScss = function () {
     gulp.src(srcp + "/scss/**/*.scss")
         .pipe(sass({
             outputStyle : "compressed",
             precision: 8,
             includePaths: [
                 'node_modules/bootstrap/scss',
-                'node_modules/@fortawesome/fontawesome-pro/scss'
             ],
         }))
         .pipe(autoprefixer({
             browsers : ["last 20 versions"]
         }))
         .pipe(gulp.dest(dest + "/css"))
-})
+}
 
-// Watch asset folder for changes
-gulp.task("watch", ["build"], function () {
-    var watcher = gulp.watch(srcp + "/scss/**/*", ["scss"])
-    watcher.on('error', function() {});
-})
+const compile = gulp.parallel([compileScss, ])
+export { compile }
 
-gulp.task("build", ["scss"])
+
+
+// // Watch asset folder for changes
+// gulp.task("watch", ["build"], function () {
+//     var watcher = gulp.watch(srcp + "/scss/**/*", ["scss"])
+//     watcher.on('error', function() {});
+// })
+
+// gulp.task("build", ["scss"])
 
 // Set watch as default task
-gulp.task("default", ["watch"])
+// gulp.task("default", ["scss"])
 
