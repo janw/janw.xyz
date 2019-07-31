4a5761f1e76e6b33a67375427d1769b546a1264a
Jan Willhaus <mail@janwillhaus.de>
2019-07-31 12:02:33 +0200

    Updated iconography

--- fa_icons_download.sh
+++ fa_icons_download.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -eu
 
-icons="light/user-astronaut light/address-card light/rss light/pen-fancy brands/mastodon brands/github brands/linkedin brands/instagram brands/flickr brands/keybase solid/envelope brands/telegram brands/apple light/heart light/copyright solid/comment-alt-dots solid/key light/comments light/ruler-combined light/door-open light/map-marker-smile light/shower light/moon-stars light/usd-circle light/flame light/people-carry light/sun light/utensils light/phone light/envelope-open-text light/bullhorn brands/whatsapp light/hand-holding-heart"
+icons="duotone/user-astronaut duotone/address-card duotone/rss duotone/pen-fancy brands/mastodon brands/github brands/linkedin brands/instagram brands/flickr brands/keybase solid/envelope brands/telegram brands/apple duotone/heart duotone/copyright solid/comment-alt-dots solid/key duotone/comments duotone/ruler-combined duotone/door-open duotone/map-marker-smile duotone/shower duotone/moon-stars duotone/usd-circle duotone/flame duotone/people-carry duotone/sun duotone/utensils duotone/phone duotone/envelope-open-text duotone/bullhorn brands/whatsapp duotone/hand-holding-heart"
 
 dest="themes/janwxyz/static/icons"
 
 GITHUB_OWNER="FortAwesome"
 GITHUB_REPO="Font-Awesome-Pro"
-repo_subdir="https://raw.githubusercontent.com/$GITHUB_OWNER/$GITHUB_REPO/master/advanced-options/raw-svg"
+repo_subdir="https://raw.githubusercontent.com/$GITHUB_OWNER/$GITHUB_REPO/master/svgs"
 
 mkdir -p "${dest}"
 cd "${dest}"
@@ -17,6 +17,7 @@ for icon in $icons; do
 
   curl --header "Authorization: token $GITHUB_API_TOKEN" \
        --header "Accept: application/vnd.github.v3.raw" \
+       --fail \
        --remote-name -J \
        --location "${repo_subdir}/${icon}"
 done
